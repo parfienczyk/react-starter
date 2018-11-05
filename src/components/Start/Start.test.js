621e906f03c8c633571699cf38037e360f6620ca
Karol P <parfienczyk@gmail.com>
2018-11-05 14:56:55 +0100

    Configure Jest and Enzyme

--- src/components/Start/Start.test.js
+++ src/components/Start/Start.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import Start from './Start';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<Start />);
 });
